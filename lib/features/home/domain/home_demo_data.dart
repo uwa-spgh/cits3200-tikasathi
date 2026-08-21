2a2bc0c3b21395abcfe78e8abf8b00aa0033ce7d
Synne Wikborg <synnejiang12@gmail.com>
2026-08-21 23:36:09 +0800

    refactor: split home models from demo data

--- lib/features/home/domain/home_demo_data.dart
+++ lib/features/home/domain/home_demo_data.dart
@@ -1,42 +1,4 @@
-import 'package:flutter/material.dart';
-
-enum HomeVaccinationGroup {
-  dueToday,
-  dueSoon,
-  upToDate,
-}
-
-@immutable
-class HomeChildSummary {
-  const HomeChildSummary({
-    required this.name,
-    required this.ageLabel,
-    required this.vaccineLabel,
-    required this.avatarEmoji,
-    required this.canRecordVaccine,
-    required this.canFindClinic,
-  });
-
-  final String name;
-  final String ageLabel;
-  final String vaccineLabel;
-  final String avatarEmoji;
-  final bool canRecordVaccine;
-  final bool canFindClinic;
-}
-
-@immutable
-class HomeStatusGroup {
-  const HomeStatusGroup({
-    required this.group,
-    required this.headerLabel,
-    required this.children,
-  });
-
-  final HomeVaccinationGroup group;
-  final String headerLabel;
-  final List<HomeChildSummary> children;
-}
+import 'home_models.dart';
 
 const List<HomeStatusGroup> homeDemoGroups = <HomeStatusGroup>[
   HomeStatusGroup(
