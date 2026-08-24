300872ff5a3afcba18df18c20ce6b84d95d5df47
Francis Merin <23744262@student.uwa.edu.au>
2026-08-24 16:01:57 +0800

    verification

--- lib/core/nip/generate.dart
+++ lib/core/nip/generate.dart
@@ -22,25 +22,33 @@ enum VaccineStatus { completed, ongoing, overdue }
 
 // minAge inclusive, maxAge exclusive
 final Map<String, List<CatchUpRule>> _catchUp = Map.unmodifiable({
-  'BCG': [
-    CatchUpRule(maxAge: DayDuration(years: 5))
-  ],
+  'BCG': [CatchUpRule(maxAge: DayDuration(years: 5))],
   'PENTA': <CatchUpRule>[], // no catch-up mentioned
-  'BOPV': [
-    CatchUpRule(doses: 3, minInterval: DayDuration(months: 1))
-  ],
-  'FIPV': [
-    CatchUpRule(doses: 2, minInterval: DayDuration(months: 4))
-  ],
+  'BOPV': [CatchUpRule(doses: 3, minInterval: DayDuration(months: 1))],
+  'FIPV': [CatchUpRule(doses: 2, minInterval: DayDuration(months: 4))],
   'ROTA': [
-    CatchUpRule(maxAge: DayDuration(years: 2), doses: 2, minInterval: DayDuration(months: 1))
+    CatchUpRule(
+        maxAge: DayDuration(years: 2),
+        doses: 2,
+        minInterval: DayDuration(months: 1))
   ],
   'PCV': [
-    CatchUpRule(maxAge: DayDuration(months: 12), doses: 3, minInterval: DayDuration(months: 1)),
-    CatchUpRule(minAge: DayDuration(months: 12), maxAge: DayDuration(months: 23), doses: 2, minInterval: DayDuration(months: 2))
+    CatchUpRule(
+        maxAge: DayDuration(months: 12),
+        doses: 3,
+        minInterval: DayDuration(months: 1)),
+    CatchUpRule(
+        minAge: DayDuration(months: 12),
+        maxAge: DayDuration(months: 23),
+        doses: 2,
+        minInterval: DayDuration(months: 2))
   ],
   'MR': [
-    CatchUpRule(minAge: DayDuration(months: 9), maxAge: DayDuration(years: 5), doses: 2, minInterval: DayDuration(months: 1))
+    CatchUpRule(
+        minAge: DayDuration(months: 9),
+        maxAge: DayDuration(years: 5),
+        doses: 2,
+        minInterval: DayDuration(months: 1))
   ],
   'JE': <CatchUpRule>[], // no catch-up mentioned
   'TCV': [
@@ -54,15 +62,14 @@ class CatchUpRule {
   final int doses;
   final DayDuration minInterval;
 
-  CatchUpRule({
-    DayDuration? minAge,
-    DayDuration? maxAge,
-    this.doses = 1,
-    DayDuration? minInterval
-  }) : 
-    minAge = minAge ?? DayDuration(),
-    maxAge = maxAge ?? DayDuration(years: 100),
-    minInterval = minInterval ?? DayDuration();
+  CatchUpRule(
+      {DayDuration? minAge,
+      DayDuration? maxAge,
+      this.doses = 1,
+      DayDuration? minInterval})
+      : minAge = minAge ?? DayDuration(),
+        maxAge = maxAge ?? DayDuration(years: 100),
+        minInterval = minInterval ?? DayDuration();
 }
 
 List<GeneratedDue> generate(
@@ -76,10 +83,17 @@ List<GeneratedDue> generate(
   _niP.forEach((vaccine, ages) {
     for (final (dose, doseAge) in ages.indexed) {
       // dose completed
-      if (records.any((AdministeredDose record) => record.vaccineCode == vaccine && record.doseNumber == dose + 1)) continue;
+      if (records.any((AdministeredDose record) =>
+          record.vaccineCode == vaccine && record.doseNumber == dose + 1)) {
+        continue;
+      }
       // dose ongoing
       if (age <= doseAge.duration) {
-        result.add((vaccineCode: vaccine, doseNumber: dose + 1, dueDate: dob.add(doseAge.duration)));
+        result.add((
+          vaccineCode: vaccine,
+          doseNumber: dose + 1,
+          dueDate: dob.add(doseAge.duration)
+        ));
         continue;
       }
       // dose overdue
@@ -92,22 +106,19 @@ List<GeneratedDue> generate(
 }
 
 VaccineStatus status(
-  DateTime today,
-  List<GeneratedDue> dues, // better name for GeneratedDue?
-  String vaccineCode
-) {
-  final vaccineDues = dues.where((GeneratedDue due) => due.vaccineCode == vaccineCode);
+    DateTime today,
+    List<GeneratedDue> dues, // better name for GeneratedDue?
+    String vaccineCode) {
+  final vaccineDues =
+      dues.where((GeneratedDue due) => due.vaccineCode == vaccineCode);
   if (vaccineDues.isEmpty) return VaccineStatus.completed;
-  final isOverdue = vaccineDues.any((GeneratedDue due) => due.dueDate.isBefore(today));
+  final isOverdue =
+      vaccineDues.any((GeneratedDue due) => due.dueDate.isBefore(today));
   return isOverdue ? VaccineStatus.overdue : VaccineStatus.ongoing;
 }
 
 List<GeneratedDue> _generateCatchUp(
-  String vaccineCode,
-  int dosesTaken,
-  Duration age,
-  DateTime today
-) {
+    String vaccineCode, int dosesTaken, Duration age, DateTime today) {
   final List<GeneratedDue> result = [];
 
   final CatchUpRule rule;
@@ -115,14 +126,22 @@ List<GeneratedDue> _generateCatchUp(
     rule = _catchUp[vaccineCode]!.firstWhere((CatchUpRule rule) {
       return rule.minAge.duration <= age && age < rule.maxAge.duration;
     });
-  } on StateError catch(_) {
+  } on StateError catch (_) {
     return result;
   }
 
   for (int i = dosesTaken; i < rule.doses; i++) {
     // For now, only vaccinations under the maxAge are added to results, which may result in incomplete vaccination schedules
-    if (age + rule.minInterval.duration * i + const Duration(days: 1) > rule.maxAge.duration) break;
-    result.add((vaccineCode: vaccineCode, doseNumber: i + 1, dueDate: today.add(rule.minInterval.duration * (i - dosesTaken) + const Duration(days: 1))));
+    if (age + rule.minInterval.duration * i + const Duration(days: 1) >
+        rule.maxAge.duration) {
+      break;
+    }
+    result.add((
+      vaccineCode: vaccineCode,
+      doseNumber: i + 1,
+      dueDate: today.add(rule.minInterval.duration * (i - dosesTaken) +
+          const Duration(days: 1))
+    ));
   }
   return result;
 }
--- test/core/nip/generate_test.dart
+++ test/core/nip/generate_test.dart
@@ -9,7 +9,10 @@ void main() {
 
       for (final due in dues) {
         expect(doesDoseExist(due.vaccineCode, due.doseNumber), true);
-        expect(today.add(getDoseAge(due.vaccineCode, due.doseNumber)!.duration) == due.dueDate, true);
+        expect(
+            today.add(getDoseAge(due.vaccineCode, due.doseNumber)!.duration) ==
+                due.dueDate,
+            true);
       }
     });
 
@@ -27,33 +30,68 @@ void main() {
 
       final dues = generate(today, today, records);
       for (final record in records) {
-        expect(dues.any((GeneratedDue due) => due.vaccineCode == record.vaccineCode && due.doseNumber == record.doseNumber), false);
+        expect(
+            dues.any((GeneratedDue due) =>
+                due.vaccineCode == record.vaccineCode &&
+                due.doseNumber == record.doseNumber),
+            false);
       }
     });
 
-    test('generate calculates the catch-up schedule for overdue vaccinations', () {
+    test('generate calculates the catch-up schedule for overdue vaccinations',
+        () {
       final today = DateTime.now();
       final dob = today.subtract(DayDuration(weeks: 12).duration);
       final records = <AdministeredDose>[
-        (vaccineCode: 'BOPV', doseNumber: 1, administeredDate: dob.add(DayDuration(weeks: 6).duration)),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 1,
+          administeredDate: dob.add(DayDuration(weeks: 6).duration)
+        ),
       ];
 
       final dues = generate(dob, today, records);
       final bopvDues = dues.where((due) => due.vaccineCode == 'BOPV');
 
-      expect(bopvDues.length, 2, reason: 'Expected 2 BOPV dues to be created, instead found ${bopvDues.length}');
-      expect(bopvDues.any((due) => due.dueDate == dob.add(getDoseAge('BOPV', 1)!.duration)), false);
-      expect(bopvDues.any((due) => due.dueDate == today.add(const Duration(days: 1))), true);
-      expect(bopvDues.any((due) => due.dueDate == today.add(DayDuration(months: 1).duration + const Duration(days: 1))), true);
+      expect(bopvDues.length, 2,
+          reason:
+              'Expected 2 BOPV dues to be created, instead found ${bopvDues.length}');
+      expect(
+          bopvDues.any(
+              (due) => due.dueDate == dob.add(getDoseAge('BOPV', 1)!.duration)),
+          false);
+      expect(
+          bopvDues
+              .any((due) => due.dueDate == today.add(const Duration(days: 1))),
+          true);
+      expect(
+          bopvDues.any((due) =>
+              due.dueDate ==
+              today.add(
+                  DayDuration(months: 1).duration + const Duration(days: 1))),
+          true);
     });
 
-    test('generate does not use catch-up schedule for completed vaccinations', () {
+    test('generate does not use catch-up schedule for completed vaccinations',
+        () {
       final today = DateTime.now();
       final dob = today.subtract(DayDuration(weeks: 16).duration);
       final records = <AdministeredDose>[
-        (vaccineCode: 'BOPV', doseNumber: 1, administeredDate: dob.add(DayDuration(weeks: 6).duration)),
-        (vaccineCode: 'BOPV', doseNumber: 2, administeredDate: dob.add(DayDuration(weeks: 10).duration)),
-        (vaccineCode: 'BOPV', doseNumber: 3, administeredDate: dob.add(DayDuration(weeks: 14).duration)),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 1,
+          administeredDate: dob.add(DayDuration(weeks: 6).duration)
+        ),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 2,
+          administeredDate: dob.add(DayDuration(weeks: 10).duration)
+        ),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 3,
+          administeredDate: dob.add(DayDuration(weeks: 14).duration)
+        ),
       ];
 
       final dues = generate(dob, today, records);
@@ -62,19 +100,30 @@ void main() {
       expect(bopvDues.length, 0);
     });
 
-    test('generate does not use catch-up schedule for ongoing vaccinations', () {
+    test('generate does not use catch-up schedule for ongoing vaccinations',
+        () {
       final today = DateTime.now();
       final dob = today.subtract(DayDuration(weeks: 9).duration);
       final records = <AdministeredDose>[
-        (vaccineCode: 'BOPV', doseNumber: 1, administeredDate: dob.add(DayDuration(weeks: 6).duration)),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 1,
+          administeredDate: dob.add(DayDuration(weeks: 6).duration)
+        ),
       ];
 
       final dues = generate(dob, today, records);
       final bopvDues = dues.where((due) => due.vaccineCode == 'BOPV');
 
       expect(bopvDues.length, 2);
-      expect(bopvDues.any((due) => due.dueDate == dob.add(getDoseAge('BOPV', 2)!.duration)), true);
-      expect(bopvDues.any((due) => due.dueDate == dob.add(getDoseAge('BOPV', 3)!.duration)), true);
+      expect(
+          bopvDues.any(
+              (due) => due.dueDate == dob.add(getDoseAge('BOPV', 2)!.duration)),
+          true);
+      expect(
+          bopvDues.any(
+              (due) => due.dueDate == dob.add(getDoseAge('BOPV', 3)!.duration)),
+          true);
     });
   });
 
@@ -83,9 +132,21 @@ void main() {
       final today = DateTime.now();
       final dob = today.subtract(DayDuration(weeks: 16).duration);
       final records = <AdministeredDose>[
-        (vaccineCode: 'BOPV', doseNumber: 1, administeredDate: dob.add(DayDuration(weeks: 6).duration)),
-        (vaccineCode: 'BOPV', doseNumber: 2, administeredDate: dob.add(DayDuration(weeks: 10).duration)),
-        (vaccineCode: 'BOPV', doseNumber: 3, administeredDate: dob.add(DayDuration(weeks: 14).duration)),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 1,
+          administeredDate: dob.add(DayDuration(weeks: 6).duration)
+        ),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 2,
+          administeredDate: dob.add(DayDuration(weeks: 10).duration)
+        ),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 3,
+          administeredDate: dob.add(DayDuration(weeks: 14).duration)
+        ),
       ];
 
       final dues = generate(dob, today, records);
@@ -96,8 +157,16 @@ void main() {
       final past = DateTime.now();
       final dob = past.subtract(DayDuration(weeks: 11).duration);
       final records = <AdministeredDose>[
-        (vaccineCode: 'BOPV', doseNumber: 1, administeredDate: dob.add(DayDuration(weeks: 6).duration)),
-        (vaccineCode: 'BOPV', doseNumber: 2, administeredDate: dob.add(DayDuration(weeks: 10).duration)),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 1,
+          administeredDate: dob.add(DayDuration(weeks: 6).duration)
+        ),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 2,
+          administeredDate: dob.add(DayDuration(weeks: 10).duration)
+        ),
       ];
 
       final dues = generate(dob, past, records);
@@ -109,12 +178,21 @@ void main() {
       final past = DateTime.now();
       final dob = past.subtract(DayDuration(weeks: 11).duration);
       final records = <AdministeredDose>[
-        (vaccineCode: 'BOPV', doseNumber: 1, administeredDate: dob.add(DayDuration(weeks: 6).duration)),
-        (vaccineCode: 'BOPV', doseNumber: 2, administeredDate: dob.add(DayDuration(weeks: 10).duration)),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 1,
+          administeredDate: dob.add(DayDuration(weeks: 6).duration)
+        ),
+        (
+          vaccineCode: 'BOPV',
+          doseNumber: 2,
+          administeredDate: dob.add(DayDuration(weeks: 10).duration)
+        ),
       ];
 
       final dues = generate(dob, past, records);
       final today = past.add(DayDuration(weeks: 4).duration);
-      expect(status(today, dues, 'BOPV'), VaccineStatus.overdue);});
+      expect(status(today, dues, 'BOPV'), VaccineStatus.overdue);
+    });
   });
 }
